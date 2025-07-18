f3b0295a13fc2a4053dc4c4de8e5d79b679d80bd
Shinwoo PARK <shinwoo.park@psw.kr>
2025-07-18 16:46:44 +0900

    feat: add proxy util

---
+++ packages/frontend/src/lib/proxy.ts
@@ -0,0 +1,40 @@
+export class ApiProxyError extends Error {
+  public readonly status: number;
+  public readonly statusText: string;
+  public readonly data?: any;
+
+  constructor(status: number, statusText: string, data?: any) {
+    super(`API Proxy Error: ${status} ${statusText}`);
+    this.status = status;
+    this.statusText = statusText;
+    this.data = data;
+  }
+}
+
+export async function proxyfetch(
+  url: string,
+  options: RequestInit
+): Promise<Response> {
+  const response = await fetch(url, options);
+  if (!response.ok) {
+    // definitely client-side http error (not backend error)
+    throw new ApiProxyError(response.status, response.statusText);
+  }
+
+  const data = await response.json();
+  if (!data.ok && data.error) {
+    // backend unexpected error
+    throw new ApiProxyError(data.status, data.error);
+  }
+
+  const r = new Response(
+    typeof data.data === "string" ? data.data : JSON.stringify(data.data),
+    {
+      status: data.status,
+      statusText: data.statusText,
+      headers: data.headers,
+    }
+  );
+
+  return r;
+}
