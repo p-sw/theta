fc5952eed8e6ba9c6138d93f4bb2d52bd242abac
Shinwoo PARK <shinwoo.park@psw.kr>
2025-07-19 19:25:43 +0900

    feat: complete Chat with AiSdk

--- packages/frontend/src/page/Chat.tsx
+++ packages/frontend/src/page/Chat.tsx
@@ -2,12 +2,24 @@ import { ModelSelector } from "@/components/block/chat/model-selector";
 import { Button } from "@/components/ui/button";
 import { Form, FormControl, FormItem, FormMessage } from "@/components/ui/form";
 import { Textarea, TextareaContainer } from "@/components/ui/textarea";
+import { useHyperId, useStorage } from "@/lib/utils";
 import { useSelectedModel } from "@/lib/storage-hooks";
+import { AiSdk } from "@/sdk";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import LucideSend from "~icons/lucide/send";
+import { SESSION } from "@/lib/const";
+import type { Session } from "@/sdk/shared";
+import { UserMessage } from "@/components/block/chat/user-message";
+import { AssistantMessage } from "@/components/block/chat/assistant-message";
+import { toast } from "sonner";
 
 export default function Chat() {
   const [[provider, modelId], setModelId] = useSelectedModel();
+  const newSessionId = useHyperId();
+  const [sessionId] = useState<string>(newSessionId);
+
+  const [session] = useStorage<Session>(SESSION(sessionId), []);
 
   const form = useForm({
     defaultValues: {
@@ -15,16 +27,40 @@ export default function Chat() {
     },
   });
 
+  const handleSubmit = (data: { message: string }) => {
+    AiSdk.message(sessionId, provider!, modelId!, [
+      { type: "text", text: data.message },
+    ]).catch((e) => {
+      toast.error(`${e.name ?? "Error"}: ${e.message}`);
+    });
+  };
+
   return (
     <main className="h-svh flex flex-col">
-      <section className="h-full overflow-y-auto p-8 flex flex-col gap-8"></section>
+      <section className="h-full overflow-y-auto p-8 flex flex-col gap-8">
+        {session.map((message) =>
+          message.type === "request" ? (
+            <UserMessage
+              key={`${sessionId}-${message.messageId}`}
+              sessionId={sessionId}
+              messageId={message.messageId}
+              messages={message.message}
+            />
+          ) : (
+            <AssistantMessage
+              key={`${sessionId}-${message.messageId}`}
+              sessionId={sessionId}
+              messageId={message.messageId}
+              messages={message.message}
+            />
+          )
+        )}
+      </section>
       <Form {...form}>
         <form
           onSubmit={(e) => {
             e.preventDefault();
-            form.handleSubmit((data) => {
-              console.log(data);
-            });
+            form.handleSubmit(handleSubmit)(e);
           }}
           className="relative p-4 h-2/5"
         >
@@ -42,7 +78,9 @@ export default function Chat() {
               <Button
                 type="submit"
                 size="icon"
-                disabled={!modelId || !form.watch("message").trim()}
+                disabled={
+                  !modelId || !provider || !form.watch("message").trim()
+                }
               >
                 <LucideSend className="size-4" />
               </Button>
